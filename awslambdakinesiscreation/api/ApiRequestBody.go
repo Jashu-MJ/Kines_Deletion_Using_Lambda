1d099a434d922979f325801ac069b5757bc46d75
jm078255 <jaswanth.marri@cerner.com>
2021-04-03 22:58:09 +0530

    deleted unwanted files

--- awslambdakinesiscreation/api/ApiRequestBody.go
+++
@@ -1,8 +0,0 @@
-//Package api contains API request body
-package api
-
-//RequestBody is the form in which the body of the request has to be.
-type RequestBody struct {
-	ClientName string `json:"name"`
-	ClientSize string `json:"size"`
-}
